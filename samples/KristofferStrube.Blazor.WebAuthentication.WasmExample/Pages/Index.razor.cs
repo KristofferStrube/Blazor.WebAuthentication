5216b3077e27531e8795725504247fa8f959f6fc
KristofferStrube <kristoffer.strube@gmail.com>
2024-09-11 21:04:53 +0200

    Added Apple to registration.

--- samples/KristofferStrube.Blazor.WebAuthentication.WasmExample/Pages/Index.razor.cs
+++ samples/KristofferStrube.Blazor.WebAuthentication.WasmExample/Pages/Index.razor.cs
@@ -89,7 +89,7 @@ public partial class Index : ComponentBase
                     Timeout = 360000,
                     Hints = ["client-device"],
                     Attestation = AttestationConveyancePreference.Direct,
-                    AttestationFormats = [AttestationFormat.Packed, AttestationFormat.AndroidKey, AttestationFormat.AndroidSafetyNet, AttestationFormat.TPM]
+                    AttestationFormats = [AttestationFormat.Packed, AttestationFormat.AndroidKey, AttestationFormat.AndroidSafetyNet, AttestationFormat.TPM, AttestationFormat.Apple]
                 }
             };
             credential = await container.CreateAsync(options) is { } c ? new PublicKeyCredential(c) : null;
